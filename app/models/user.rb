31e3648b8ccd3077f6b6dd679c752c6747abe47e
jkirky82 <jkirky82@gmail.com>
2021-12-08 13:14:38 +1000

    Updated active storage to allow a seeker to upload a resume and for it to be stored

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :seeker
+  
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
