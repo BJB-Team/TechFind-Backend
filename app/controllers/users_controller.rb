f29382137335d2abad9554a2ddfc50f07d3a7726
jkirky82 <jkirky82@gmail.com>
2022-01-12 23:36:21 +1000

    added login validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
   def sign_in
       @user = User.find_by_email(params[:email])
-      puts @user
       if @user && @user.authenticate(params[:password])
           auth_token = Knock::AuthToken.new payload: {sub: @user.id}
           render json: {username: @user.username, jwt: auth_token.token, id: @user.id, account_seeker: @user.account_seeker}, status: 201
