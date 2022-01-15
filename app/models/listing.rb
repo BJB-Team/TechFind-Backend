0e0563d8a1ad23d309654342e1cc23fee51a4948
Jack Kirkwood <jkirky82@gmail.com>
2022-01-15 18:02:22 +1000

    edit and delete for user

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,6 @@ class Listing < ApplicationRecord
   belongs_to :user 
   belongs_to :job_level
   belongs_to :job_type
-  has_many :application
+
+  has_many :application, dependent: :delete_all
 end
