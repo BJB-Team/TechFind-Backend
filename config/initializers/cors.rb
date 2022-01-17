99c9faa32690f6cc2f95c141eadd1e8deabd6040
Jack Kirkwood <jkirky82@gmail.com>
2022-01-17 15:18:36 +1000

    heroku deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000','127.0.0.1:3000'
+    origins 'localhost:8000','127.0.0.1:3000', 'http://techfind.software.com','https://upbeat-dubinsky-90fa31.netlify.app/'
 
     resource '*',
       headers: :any,
